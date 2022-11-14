fbda7f6f7a300b8098d695a786b3205a0cd890a5
celestefr <celeste.francier@protonmail.com>
2022-11-15 00:14:25 +0100

    ft_printf 15/11/22

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: cfrancie <cfrancie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 13:45:16 by cfrancie          #+#    #+#             */
-/*   Updated: 2022/11/10 15:32:08 by cfrancie         ###   ########.fr       */
+/*   Updated: 2022/11/15 00:12:22 by cfrancie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,27 @@
 
 int	ft_printf(const char *str, ...)
 {
-	size_t	i;
+	va_list		ap;
+	int			i;
 
-	if (!str);
-		return (0)
 	i = 0;
+	va_start(ap, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			if (str[i] == 'd')
+				ft_putnbr(va_arg(ap, int));
+			else if (str[i] == 's')
+				ft_putstr(va_arg(ap, char *));
+			else if (str[i] == 'c')
+				ft_putchar(va_arg(ap, int));
+		}
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
+	va_end(ap);
+	return (0);
 }
