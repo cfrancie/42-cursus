f7a55f4b6ab3288d357cb1f45ff06dc76221366c
cfrancie <celeste.francier@protonmail.com>
2023-01-03 03:04:44 +0100

    libft 03/01/23

--- libft/srcs/ft_atof.c
+++ libft/srcs/ft_atof.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atof.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cfrancie <cfrancie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cfrancie@student.42.fr <cfrancie>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 02:04:19 by cfrancie          #+#    #+#             */
-/*   Updated: 2022/12/14 19:00:09 by cfrancie         ###   ########.fr       */
+/*   Updated: 2023/01/02 20:52:23 by cfrancie@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,31 @@
 
 double	ft_atof(const char *str)
 {
-	double	res;
-	double	dec;
+	double	nb;
+	double	nb2;
 	int		i;
-	int		sign;
+	int		j;
 
+	nb = 0;
+	nb2 = 0;
 	i = 0;
-	res = 0;
-	dec = 0;
-	sign = 1;
+	j = 0;
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
 		|| str[i] == '\f' || str[i] == '\r')
 		i++;
 	if (str[i] == '-')
-		sign = -1;
+		j = 1;
 	if (str[i] == '-' || str[i] == '+')
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
-		res = res * 10 + str[i++] - '0';
+		nb = nb * 10 + str[i++] - '0';
 	if (str[i] == '.')
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
-	{
-		dec = dec * 10 + str[i++] - '0';
-		res += dec / 10;
-	}
-	return (res * sign);
+		nb2 = nb2 * 10 + str[i++] - '0';
+	while (nb2 >= 1)
+		nb2 /= 10;
+	if (j == 1)
+		return ((nb + nb2) * -1);
+	return (nb + nb2);
 }
