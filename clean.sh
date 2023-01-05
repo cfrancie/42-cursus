1cbaded78337492e8579c7be2cb719f997ac7f97
cfrancie <celeste.francier@protonmail.com>
2023-01-05 16:36:19 +0100

    clean.sh

--- clean.sh
+++ clean.sh
@@ -1,12 +1,3 @@
 #/bin/sh
 
-function clean {
-	make clean -C $1
-}
-
-clean fract-ol
-clean ft_printf
-clean get_next_line
-clean libft
-clean pipex
-clean push_swap
+find . -mindepth 1 -maxdepth 1 -type d -exec bash -c "cd '{}' && make fclean" \;
